66882b98ea55e6c77e1229e1df6e749660342a85
gdek <gdekeyne@extia.be>
2021-03-01 17:49:24 +0100

    update install

--- installation.cmd
+++ installation.cmd
@@ -1,7 +1,7 @@
 pip install -r requirements.txt
 pyinstaller --onefile --icon=static\YAd_icon.ico lua_autocad_comparator.py
-mv dist\* .
-rm *.spec
+move dist\* .
+del *.spec
 rmdir dist
-rmdir __pycache__ /s
-rmdir build /s
+rmdir __pycache__ /s /q
+rmdir build /s /q
